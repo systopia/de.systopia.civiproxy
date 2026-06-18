b811e2b772ec208407863c180150e168cfc4af1e
Jens Schuppe <schuppe@systopia.de>
2026-06-18 13:46:26 +0200

    Set version to 1.1.0-beta2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.civiproxy',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4ee7825f6f2e3626692465bd46cff87fcbcb028d',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '1.1.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.civiproxy' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4ee7825f6f2e3626692465bd46cff87fcbcb028d',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '1.1.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
